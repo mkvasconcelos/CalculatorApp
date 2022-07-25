b09f40ce5102354d90f10ea831fc69b1d715e296
mkvasconcelos <mateuskvasconcelos@gmail.com>
2022-07-25 20:41:25 -0300

    Arquivo js com funções para a calculadora

---
+++ js/main.js
@@ -0,0 +1,27 @@
+function addition(a, b) {
+    return a + b
+}
+
+function subtraction(a, b) {
+    return a - b
+}
+
+function multiplication(a, b) {
+    return a * b
+}
+
+function division(a, b) {
+    return a / b
+}
+
+function square(a) {
+    return a ** 2
+}
+
+function potentiation(a, b) {
+    return a * b
+}
+
+function squareRoot(a) {
+    return a ** 0.5
+}
