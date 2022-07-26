e2065d70cffde0b57cb53684c262fdf7e076ad25
mkvasconcelos <mateuskvasconcelos@gmail.com>
2022-07-26 18:43:52 -0300

    Operações matemáticas funcionando

--- js/main.js
+++ js/main.js
@@ -1,25 +1,54 @@
-function addition(a, b) {
-    return a + b
+let operator = 0
+let number1 = 0
+let number2 = 0
+
+function button(button) {
+    if (operator === 0) {
+        if (document.getElementById("display").innerHTML == 0) {
+            document.getElementById("display").innerHTML = button
+        } else {
+            document.getElementById("display").innerHTML = document.getElementById("display").innerHTML.concat(button)
+        }
+    } else {
+        document.getElementById("display").innerHTML = button
+    }
+    // document.getElementById("display").innerHTML = button
+    number2 = document.getElementById("display").innerHTML
+    return number2
 }
 
-function subtraction(a, b) {
-    return a - b
+function prevButton() {
+    number1 = document.getElementById("display").innerHTML
+    return number1
 }
 
-function multiplication(a, b) {
-    return a * b
+function oper(oper) {
+    if (operator === 0) {
+        operator = oper
+    } else {
+        result(number1, number2, operator)
+        operator = oper
+    }
+    return operator
 }
 
-function division(a, b) {
-    return a / b
+function result(num1, num2, oper) {
+    answer = eval(num1 + oper + num2)
+    document.getElementById("display").innerHTML = answer
+    operator = 0
+    return operator
 }
 
-function inverse(a) {
-    return -a
+function inverse() {
+    document.getElementById("display").innerHTML *= -1
 }
 
-function percentage(a) {
-    return a / 100
+function percentage() {
+    document.getElementById("display").innerHTML /= 100
 }
 
-
+function reset() {
+    document.getElementById("display").innerHTML = 0
+    number1, number2, operator = 0, 0, 0
+    return number1, number2, operator
+}
