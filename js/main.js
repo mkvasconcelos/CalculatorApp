06113e13f4806e777debcd9194eb3f0c765a652a
mkvasconcelos <mateuskvasconcelos@gmail.com>
2022-07-27 11:37:55 -0300

    JS funcionando por completo

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
-let operator = 0
-let number1 = 0
-let number2 = 0
-let answer = 0
-let clicks = 0
+var operator = 0
+var number1 = 0
+var number2 = 0
+var answer = 0
+var clicks = 0
 
 function button(button) {
     if (button === "." && document.getElementById("display").innerHTML.indexOf(".") != -1) {
@@ -66,21 +66,30 @@ function result(num1, num2, oper) {
     }
     answer = eval(num1 + oper + num2)
     document.getElementById("display").innerHTML = answer
-    number1 = answer
+    number2 = answer
     operator = 0
+    clicks = 0
     return operator, answer, number1
 }
 
 function inverse() {
     document.getElementById("display").innerHTML *= -1
+    number2 = document.getElementById("display").innerHTML
+    clicks = 0
 }
 
 function percentage() {
     document.getElementById("display").innerHTML /= 100
+    number2 = document.getElementById("display").innerHTML
+    clicks = 0
 }
 
 function reset() {
     document.getElementById("display").innerHTML = 0
-    number1, number2, operator, clicks, answer = 0, 0, 0
+    number1 = 0
+    number2 = 0
+    operator = 0
+    clicks = 0
+    answer = 0
     return number1, number2, operator, clicks, answer
 }
